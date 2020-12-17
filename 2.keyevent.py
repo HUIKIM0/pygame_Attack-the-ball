c9296dce368e3eb6268a9d9c2b664d8d30000a38
HUIKIM0 <fehs123@naver.com>
2020-12-17 20:29:19 +0900

    upload_keyevent

--- 2.keyevent.py
+++ 2.keyevent.py
@@ -14,7 +14,7 @@ pygame.display.set_caption("!공을 맞춰라!") # 게임 타이틀
 clock = pygame.time.Clock()
 
 
-# 1. 사용자 게임 초기화 (배경 화면, 게임 이미지, 좌표, 속도, 폰트 등)
+# ★1. 사용자 게임 초기화 (배경 화면, 게임 이미지, 좌표, 속도, 폰트 등)
 # os.path.dirname(__file__)
 # os.path.join(기존경로,새로운폴더이름) => 합쳐서 하위경로 만드는거
 current_path = os.path.dirname(__file__)  # 현재 파일의 위치 반환(코딩 하고있는 이 파일 위치)
@@ -44,8 +44,9 @@ character_height = character_size[1]
 character_x_pos = (screen_width/2) - (character_width/2)
 character_y_pos = screen_height - character_height - stage_height # 스테이지도 빼야 스테이지 위에 캐릭터가 놓임
 
-# 캐릭터 이동 방향 (가로로만 움직임)
-character_to_x = 0
+
+# 캐릭터 이동 방향 (가로로만 움직임). keyevent를 위함!!
+character_go_x = 0
 
 # 캐릭터의 이동 속도
 character_speed = 5
@@ -71,21 +72,54 @@ running = True  # 게임이 진행중인가?
 while running:
     dt = clock.tick(30)  # 게임화면의 초당 프레임 수를 설정
 
-    # 2. 이벤트 처리 (키보드,마우스 등)
+    # ★2. 이벤트 처리 (키보드,마우스 등)
     for event in pygame.event.get():   # 사용자로 인한 동작이 들어오는지 체크
         if event.type == pygame.QUIT:  # 만약 사용자가 창을 닫으면
             running = False  # 게임 진행중 X
+        
+        # ●키보드에 입력 들어옴
+        if event.type == pygame.KEYDOWN:  
+            if event.key == pygame.K_LEFT:
+                character_go_x -= character_speed
+            elif event.key == pygame.K_RIGHT:
+                character_go_x += character_speed
 
+            elif event.type == pygame.K_SPACE: # 무기 발사 
+                weapon_x_pos = character_x_pos + (character_width/2)  # 무기는 캐릭터의 중앙에서 나오게
+                weapon_y_pos = character_y_pos  # 캐릭터의 바로 위에서 발사되게
+                weapons.append = ([weapon_x_pos, weapon_y_pos])  # list의 형태로 weapons list에 x좌표 y좌표 추가
+                                                                 # 무기를 여러번 쏘면 쏠수록 이런 값들이 계속 추가됨
+                                                                
+        # ●사용자가 손을 땜
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+               character_go_x = 0
 
+                
     
-    # 3. 게임 캐릭터 위치 정의
+    # ★3. 게임 캐릭터 위치 정의
+    character_x_pos += character_go_x  # x위치에 키보드 입력으로 인해 변한 값을 더해줌(캐릭터 위치 지정)
+    
+    # 캐릭터가 화면에 벗어나지 않게 처리
+    if character_x_pos < 0:
+        character_x_pos = 0
+    elif character_x_pos > screen_width - character_width:   # 캐릭터위치 > 607. 화면을 벗어났음
+        character_x_pos = screen_width - character_width     
 
-    # 4. 충돌 처리
 
-    # 5. 화면에 그리기
+    # 공격시 무기 위치 위로 올라가게 변경
+    
+
+    # ★4. 충돌 처리
+
+    # ★5. 화면에 그리기
     screen.blit(background,(0,0))
     screen.blit(stage,(0,(screen_height-stage_height))) # 0,430
     screen.blit(character,(character_x_pos,character_y_pos))
+
+    # list weapons에 담긴 개수 만큼 스크린에 그려준다
+    for weapon_x_pos,weapons_y_pos in weapons:
+        screen.blit(weapons,(weapon_x_pos,weapon_y_pos))
 
     pygame.display.update()
 
